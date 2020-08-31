0e09d66ff8e26dfdbe6d900838d3805c3bf69a8c
Alejandro Mildiner <alejandro.mildiner@gmail.com>
2020-08-31 13:07:14 -0300

    small refactor improvement

--- tests/test_constants.py
+++ tests/test_constants.py
@@ -4,4 +4,4 @@ DUMMY_VALID_NAME = "dummyValidName"
 DUMMY_DATE = "2020-04-21T18:25:43"
 GEOMETRY = {"type": "Polygon", "coordinates": [[[0, 0], [10, 0], [10, 10], [0, 10], [0, 0]]]}
 OPEN_GEOMETRY = {"type": "Polygon", "coordinates": [[[0, 0], [10, 0], [10, 10], [0, 10], [0, 1]]]}
-INVALID_GEOMETRY = {"type": "Polygon", "coordinates": [[[0, 0], [10, 0], [10, 10], [0, 10], ['a', 0]]]}
+INVALID_GEOMETRY = {"type": "Polygon34", "coordinates": [[[0, 0], [10, 0], [10, 10], [0, 10], [1, 0]]]}
